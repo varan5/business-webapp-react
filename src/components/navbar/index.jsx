148bdf377a3c78a4ab435392cc5b96e8b4f87d0e
varan5 <varan2.bhalla@gmail.com>
2021-07-13 03:28:34 +0530

    Business Website Template

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Button } from '../button'
+import { Logo } from '../logo'
+import { Marginer } from '../marginer'
+import { scroller } from 'react-scroll'
+
+const NavbarContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  padding: 0 2em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const BrandContainer = styled.div``
+
+const AccessibilityContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const LoginButton = styled(Button)`
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    background-color: transparent;
+    border: none;
+    color: rgb(212, 212, 212);
+  }
+`
+const scrollToNextSection = () => {
+  scroller.scrollTo('servicesSection', { smooth: true, duration: 1500 })
+}
+export function Navbar(props) {
+  return (
+    <NavbarContainer>
+      <BrandContainer>
+        <Logo inline />
+      </BrandContainer>
+      <AccessibilityContainer>
+        <Button small onClick={scrollToNextSection}>
+          Get Started
+        </Button>
+        <Marginer direction="horizontal" margin="8px" />
+        <LoginButton small>Login</LoginButton>
+      </AccessibilityContainer>
+    </NavbarContainer>
+  )
+}
